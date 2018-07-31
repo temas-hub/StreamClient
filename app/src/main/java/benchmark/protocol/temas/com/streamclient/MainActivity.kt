f106e2c638cf7e23d9cffb58e17576848e75c950
Artem Zhdanov <artem.zhdanov@kohls.com>
2018-07-31 23:30:08 +0200

    Initial commit

---
+++ app/src/main/java/benchmark/protocol/temas/com/streamclient/MainActivity.kt
@@ -0,0 +1,24 @@
+package benchmark.protocol.temas.com.streamclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.temas.protocols.benchmark.udp.UdpClient
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("streamClient","Start Activity");
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        try {
+            Log.d("streamClient","Creating UDP Client");
+            val udpClient = UdpClient("192.168.1.7", 11100)
+            Log.d("streamClient","Created...");
+            udpClient.init()
+            Log.d("streamClient","Init's done");
+        } catch (e: Exception) {
+            Log.e("streamClient", e.message, e)
+        }
+    }
+}
